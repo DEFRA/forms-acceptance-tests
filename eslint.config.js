73886f059400f7cad00411956a5a36d7db46840c
Alex Luckett <alex@nodepoint.co.uk>
2026-06-17 12:51:06 +0100

    chore: restore import rules via eslint-plugin-import-x
    neostandard does not include import rules by default, which dropped 6
    rules that eslint-config-standard previously enforced. Re-add them
    explicitly via eslint-plugin-import-x (the import-x fork of
    eslint-plugin-import, compatible with eslint v9):
    
      import-x/export, import-x/first, import-x/no-absolute-path,
      import-x/no-duplicates, import-x/no-named-default,
      import-x/no-webpack-loader-syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import neostandard from 'neostandard'
+import importX from 'eslint-plugin-import-x'
 import wdioPlugin from 'eslint-plugin-wdio'
 import prettierPlugin from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
@@ -17,6 +18,17 @@ export default [
   },
   ...neostandard({ noStyle: true }),
   wdioPlugin.configs['flat/recommended'],
+  {
+    plugins: { 'import-x': importX },
+    rules: {
+      'import-x/export': 'error',
+      'import-x/first': 'error',
+      'import-x/no-absolute-path': 'error',
+      'import-x/no-duplicates': 'error',
+      'import-x/no-named-default': 'error',
+      'import-x/no-webpack-loader-syntax': 'error'
+    }
+  },
   {
     plugins: { prettier: prettierPlugin },
     languageOptions: {
